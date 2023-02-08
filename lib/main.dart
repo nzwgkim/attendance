42793ebe3c774f4fc01b67ec663074093a76ed92
Woody <nz.wgkim@gmail.com>
2023-02-09 05:15:11 +1300

    Major functions completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -31,24 +32,288 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //GPS coordinates of Massey University, New Zealand. Latitude: -41.3009 Longitude: 174.7720. :
-  static const LatLng schoolLatLng = LatLng(-41.3009, 174.7720);
-  static const LatLng companyLatLng = LatLng(37.5233273, 126.921252);
+  bool choolCheckDone = false;
+  GoogleMapController? mapController;
 
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-  static const CameraPosition initPos =
-      CameraPosition(target: schoolLatLng, zoom: 15.0);
+  // latitude - 위도 , longitude - 경도
+  static const LatLng masseyUniLatLng = LatLng(-36.7327, 174.7016);
+
+  static const LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
+  static const LatLng targetLatLng = masseyUniLatLng;
+
+  static const CameraPosition initialPosition = CameraPosition(
+    target: targetLatLng,
+    zoom: 15,
+  );
+  static const double okDistance = 100;
+  static final Circle withinDistanceCircle = Circle(
+    circleId: const CircleId('withinDistanceCircle'),
+    center: targetLatLng,
+    fillColor: Colors.blue.withOpacity(0.5),
+    radius: okDistance,
+    strokeColor: Colors.blue,
+    strokeWidth: 1,
+  );
+  static final Circle notWithinDistanceCircle = Circle(
+    circleId: const CircleId('notWithinDistanceCircle'),
+    center: targetLatLng,
+    fillColor: Colors.red.withOpacity(0.5),
+    radius: okDistance,
+    strokeColor: Colors.red,
+    strokeWidth: 1,
+  );
+  static final Circle checkDoneCircle = Circle(
+    circleId: const CircleId('checkDoneCircle'),
+    center: targetLatLng,
+    fillColor: Colors.green.withOpacity(0.5),
+    radius: okDistance,
+    strokeColor: Colors.green,
+    strokeWidth: 1,
+  );
+  static const Marker marker = Marker(
+    markerId: MarkerId('marker'),
+    position: targetLatLng,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-          // mapType: MapType.hybrid,
-          initialCameraPosition: initPos,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          }),
+      appBar: renderAppBar(),
+      body: FutureBuilder<String>(
+        future: checkPermission(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // 위치 권한이 허가 되었습니다.
+          if (snapshot.data == 'Permitted to get the position') {
+            return StreamBuilder<Position>(
+              stream: Geolocator.getPositionStream(),
+              builder: (context, snapshot) {
+                bool isWithinRange = false;
+
+                if (snapshot.hasData) {
+                  final start = snapshot.data!;
+                  const end = targetLatLng;
+
+                  final distance = Geolocator.distanceBetween(
+                    start.latitude,
+                    start.longitude,
+                    end.latitude,
+                    end.longitude,
+                  );
+
+                  if (distance < okDistance) {
+                    isWithinRange = true;
+                  }
+                }
+
+                return Column(
+                  children: [
+                    _CustomGoogleMap(
+                      initialPosition: initialPosition,
+                      circle: choolCheckDone
+                          ? checkDoneCircle
+                          : isWithinRange
+                              ? withinDistanceCircle
+                              : notWithinDistanceCircle,
+                      marker: marker,
+                      onMapCreated: onMapCreated,
+                    ),
+                    _ChoolCheckButton(
+                      isWithinRange: isWithinRange,
+                      choolCheckDone: choolCheckDone,
+                      onPressed: onChoolCheckPressed,
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+
+          return Center(
+            child: Text(snapshot.data),
+          );
+        },
+      ),
+    );
+  }
+
+  onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  onChoolCheckPressed() async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Attendance'),
+          content: const Text('Are you ready to work?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result) {
+      setState(() {
+        choolCheckDone = true;
+      });
+    }
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      // 위치 서비스를 활성화 해주세요.
+      return 'Please activate the position service';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        // 위치 권한을 허가해주세요.
+        return 'Please allow to get position privilege';
+      }
+    }
+
+    if (checkedPermission == LocationPermission.deniedForever) {
+      // 앱의 위치 권한을 세팅에서 허가해주세요.
+      return 'You can change this app for the position at the Settings';
+    }
+
+    // 위치 권한이 허가 되었습니다.
+    return 'Permitted to get the position';
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      title: const Text(
+        'Attendance',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () async {
+            if (mapController == null) {
+              return;
+            }
+
+            final location = await Geolocator.getCurrentPosition();
+
+            mapController!.animateCamera(
+              CameraUpdate.newLatLng(
+                LatLng(
+                  location.latitude,
+                  location.longitude,
+                ),
+              ),
+            );
+          },
+          color: Colors.blue,
+          icon: const Icon(
+            Icons.my_location,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+  final Circle circle;
+  final Marker marker;
+  final MapCreatedCallback onMapCreated;
+
+  const _CustomGoogleMap({
+    required this.initialPosition,
+    required this.circle,
+    required this.marker,
+    required this.onMapCreated,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        circles: {circle},
+        markers: {marker},
+        onMapCreated: onMapCreated,
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  final bool isWithinRange;
+  final VoidCallback onPressed;
+  final bool choolCheckDone;
+
+  const _ChoolCheckButton({
+    required this.isWithinRange,
+    required this.onPressed,
+    required this.choolCheckDone,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.timelapse_outlined,
+            size: 50.0,
+            color: choolCheckDone
+                ? Colors.green
+                : isWithinRange
+                    ? Colors.blue
+                    : Colors.red,
+          ),
+          const SizedBox(height: 20.0),
+          if (!choolCheckDone && isWithinRange)
+            TextButton(
+              onPressed: onPressed,
+              child: const Text('Attendance'),
+            ),
+        ],
+      ),
     );
   }
 }
